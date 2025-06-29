1176a159b20dced33896efbe1bb8edf6579cfe28
Imbirel <katelin.kzm@gmail.com>
2025-06-29 21:52:15 +0300

    navigation and redirect

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,21 @@
-import { UserProvider } from "@/components/UserProvider/UserProvider";
 import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <UserProvider>
-          <ul>
-            <li>
-              <Link href="/notes">Notes</Link>
-            </li>
-            <li>
-              <Link href="/user">User</Link>
-            </li>
-            <li>
-              <Link href="/subscription">Subscription</Link>
-            </li>
-          </ul>
-          {children}
-        </UserProvider>
+        <ul>
+          <li>
+            <Link href="/notes">Notes</Link>
+          </li>
+          <li>
+            <Link href="/user">User</Link>
+          </li>
+          <li>
+            <Link href="/subscription">Subscription</Link>
+          </li>
+        </ul>
+        {children}
       </body>
     </html>
   );
