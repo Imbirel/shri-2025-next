ff64776e2d56eba6b25254891c0cf9a0f647331d
Imbirel <katelin.kzm@gmail.com>
2025-07-02 18:22:21 +0300

    adding SSR with caching

--- frontend/src/api/note.js
+++ frontend/src/api/note.js
@@ -2,6 +2,12 @@ export async function getNotes() {
   return (await fetch("http://localhost:3001/notes")).json();
 }
 
+export async function getTopNotes() {
+  const notes = await (await fetch("http://localhost:3001/notes")).json();
+
+  return [notes[0], notes[3], notes[6]];
+}
+
 export async function getNote(id) {
   return (await fetch(`http://localhost:3001/notes/${id}`)).json();
 }
--- frontend/src/app/note/[noteId]/page.js
+++ frontend/src/app/note/[noteId]/page.js
@@ -1,12 +1,18 @@
 import { getNote } from "@/api/note";
 import { Note } from "@/components/Note";
 
+export async function generateStaticParams() {
+  return (await getTopNotes()).map(({ id }) => ({
+    noteId: id,
+  }));
+}
+
 export default async function NotePage({ params }) {
   const note = await getNote(params.noteId);
 
-  if(!note) {
-    return <div>No Note</div>
+  if (!note) {
+    return <div>No Note</div>;
   }
 
-  return <Note {...note}/>;
+  return <Note {...note} />;
 }
